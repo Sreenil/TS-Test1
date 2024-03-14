5c895a1e127cde07c0f12a8ab2c1886fc89d0eec
Sreenil <sreenilankath@gmail.com>
2024-03-14 15:31:04 +0530

    first commit

---
+++ TypeScript1/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { log } from 'node:console';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'TypeScript1';
+
+  ngOnInit(): void {
+    
+    this.example();
+  }
+  
+  example() {
+    var variable1 = 'Hello';
+    let variable2 = 'World';
+    const variable3 = 'Welcome';
+    let variable4 = 'Jhon';
+    
+    if(true) {
+      var variable1 = 'Goodbye';
+      let variable2 = 'Universe';
+      let variable4 = 'Doe';
+      
+      console.log('1', variable1)
+      console.log('2', variable2)
+      console.log('3', variable3)
+      console.log('4', variable4)
+    }
+    
+    console.log('5', variable1)
+    console.log('6', variable2)
+    console.log('7', variable3)
+    console.log('8', variable4)
+  }
+}
